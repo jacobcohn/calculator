21b71128ece335baafe70aa0923d2bdbf5847879
Jacob Cohn <jacobnc916@gmail.com>
2021-04-27 23:28:33 -0500

    working display

--- script.js
+++ script.js
@@ -13,7 +13,24 @@ function operate(operator, a, b) {
         case 'divide':
             num = a / b;
             break;
-    }
+    };
     num = Math.round(num * 1000) / 1000;
     return num;
-}
+};
+
+const display = document.querySelector('#display');
+const numbers = Array.prototype.slice.call(document.querySelectorAll('#numbers-div > button'));
+const displayArray = [];
+let currentDisplay;
+
+numbers.forEach(item => {
+    item.addEventListener('click', e => {
+        if (e.target.value === '.' && displayArray.includes('.')) {
+           return; 
+        };
+        displayArray.push(e.target.value);
+        currentDisplay = displayArray.join('');
+        display.innerHTML = currentDisplay;
+    });
+});
+
